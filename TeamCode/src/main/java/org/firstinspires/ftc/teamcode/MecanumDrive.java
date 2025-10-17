bf6d13afffb3be7971df63d5234e3206871d2e00
WizardCat432 <amirelijah2016@icloud.com>
2025-10-17 17:07:32 -0600

    tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -82,7 +82,7 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
+        public double axialGain = 5.0;
         public double lateralGain = 5.0;
         public double headingGain = 10.0; // shared with turn
 
